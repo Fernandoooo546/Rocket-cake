4bcc6b6b7f4eabb5f6c5b7bc951b0a1f7dba985b
Fernandoooo546 <fpere0009@gmail.com>
2024-08-24 18:42:35 -0600

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="generator" content="RocketCake">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index3_php.css">
+</head>
+<body>
+<div class="textstyle1">
+<div id="container_139fc2a5"><div id="container_139fc2a5_padding" ><div class="textstyle1">  <div id="menu_4eccecbb">
+    <div  class="menuholder1"><a href="javascript:void(0);">
+	<div id="menuentry_15cc3a3f"  class="menustyle1 menu_4eccecbb_mainMenuEntry mobileEntry">
+		<div class="menuentry_text1">
+      <span class="textstyle2">Menu &#9660;</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_fd68664"  class="menustyle1 menu_4eccecbb_mainMenuEntry normalEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle3">ITEGT</span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_63b602b"  class="menustyle2 menu_4eccecbb_mainMenuEntry normalEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle3">MODALIDADES</span>
+		</div>
+	</div>
+</a>
+<a href="index1.html" style="text-decoration:none">
+	<div id="menuentry_50e02a7f"  class="menustyle3 menu_4eccecbb_mainMenuEntry normalEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle3">Galer&#237;a</span>
+		</div>
+	</div>
+</a>
+<a href="index2.html" style="text-decoration:none">
+	<div id="menuentry_25f77e0e"  class="menustyle4 menu_4eccecbb_mainMenuEntry normalEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle3">Videos </span>
+		</div>
+	</div>
+</a>
+<a href="javascript:void(0);">
+	<div id="menuentry_d25f5"  class="menustyle5 menu_4eccecbb_mainMenuEntry normalEntry">
+		<div class="menuentry_text2">
+      <span class="textstyle3">Contactos</span>
+		</div>
+	</div>
+</a>
+
+	<script type="text/javascript" src="rc_images/wsp_menu.js"></script>
+	<script type="text/javascript">
+		var js_menu_4eccecbb= new wsp_menu('menu_4eccecbb', 'menu_4eccecbb', 10, null, true);
+
+		js_menu_4eccecbb.createMenuForItem('menuentry_15cc3a3f', ["      <span class=\"textstyle4\">ITEGT</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Direcci&#243;n</span> ", 'index.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Subdirecci&#243;n</span> ", 'about.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Secretar&#237;a</span> ", 'contact.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Consejer&#237;a </span> ", 'sin_t_tulo.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Administraci&#243;n</span> ", 'sin_t_tulo31.html', '',
+		                                   "      <span class=\"textstyle4\">MODALIDADES</span> ", 'javascript:void(0);', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;Ciclo B&#225;sico T&#233;cnico</span> ", 'sin_t_tulo3.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;D&#233;cimo (BTP)</span> ", 'sin_t_tulo4.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Inform&#225;tica</span> ", 'sin_t_tulo5.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Contadur&#237;a y Finanzas</span> ", 'sin_t_tulo6.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Mec&#225;nica Industrial</span> ", 'sin_t_tulo7.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Mec&#225;nica Automotriz</span> ", 'sin_t_tulo8.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Administraci&#243;n de Empresas</span> ", 'sin_t_tulo9.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Administraci&#243;n Hotelera</span> ", 'sin_t_tulo10.html', '',
+		                                   "      <span class=\"textstyle4\">&#160;&#160;&#160;BTP Refrigeraci&#243;n y Aire Acondicionado</span> ", 'sin_t_tulo11.html', '',
+		                                   "      <span class=\"textstyle4\">Galer&#237;a</span> ", 'index1.html', '',
+		                                   "      <span class=\"textstyle4\">Videos </span> ", 'index2.html', '',
+		                                   "      <span class=\"textstyle4\">Contactos</span> ", 'javascript:void(0);', '']);
+		js_menu_4eccecbb.createMenuForItem('menuentry_fd68664', ["      <span class=\"textstyle3\">Direcci&#243;n</span> ", 'index.html', '',
+		                                   "      <span class=\"textstyle3\">Subdirecci&#243;n</span> ", 'about.html', '',
+		                                   "      <span class=\"textstyle3\">Secretar&#237;a</span> ", 'contact.html', '',
+		                                   "      <span class=\"textstyle3\">Consejer&#237;a </span> ", 'sin_t_tulo.html', '',
+		                                   "      <span class=\"textstyle3\">Administraci&#243;n</span> ", 'sin_t_tulo31.html', '']);
+		js_menu_4eccecbb.createMenuForItem('menuentry_63b602b', ["      <span class=\"textstyle3\">Ciclo B&#225;sico T&#233;cnico</span> ", 'sin_t_tulo3.html', '',
+		                                   "      <span class=\"textstyle3\">D&#233;cimo (BTP)</span> ", 'sin_t_tulo4.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Inform&#225;tica</span> ", 'sin_t_tulo5.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Contadur&#237;a y Finanzas</span> ", 'sin_t_tulo6.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Mec&#225;nica Industrial</span> ", 'sin_t_tulo7.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Mec&#225;nica Automotriz</span> ", 'sin_t_tulo8.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Administraci&#243;n de Empresas</span> ", 'sin_t_tulo9.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Administraci&#243;n Hotelera</span> ", 'sin_t_tulo10.html', '',
+		                                   "      <span class=\"textstyle3\">BTP Refrigeraci&#243;n y Aire Acondicionado</span> ", 'sin_t_tulo11.html', '']);
+		js_menu_4eccecbb.createMenuForItem('menuentry_50e02a7f', []);
+		js_menu_4eccecbb.createMenuForItem('menuentry_25f77e0e', []);
+		js_menu_4eccecbb.createMenuForItem('menuentry_d25f5', []);
+
+	</script>
+      </div>
+    </div>
+  <span class="textstyle5"><br/><br/><br/><br/></span>
+<h1 id="heading_9cc5391">INSTITUTO T&#201;CNICO EULOGIO GALEANO TREJO</h1><span class="textstyle6"><br/></span><span class="textstyle7">ESTUDIO DISCIPLINA TRABAJO</span></div>
+<div class="textstyle8">  <img src="rc_images/descarga.jpg" width="225" height="225" id="img_6a66e3d4" alt="" title="" />
+</div>
+<div style="clear:both"></div></div></div><div id="container_6b2e8229"><div id="container_6b2e8229_padding" ></div></div><div id="container_5d2ad265"><div class="textstyle1"><div id="container_3b1b6666"><div id="container_3b1b6666_padding" ><div class="textstyle1">  <span class="textstyle9"><br/></span>
+  <div id="button_47b8a20e">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle10">FORMULARIO DE CONTACTO</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/>Escribenos y en breve los pondremos en contacto contigo<br/><br/></span>
+  <div id="button_54a45994">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle12">Nombre</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/></span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_4d2d62e5" >
+  <span class="textstyle11"><br/>Escribe el nombre<br/><br/></span>
+  <div id="button_799a412f">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle12">Email</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/></span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_3c8fcf7b" >
+  <span class="textstyle11"><br/>Escribe tu Email<br/><br/></span>
+  <div id="button_515c7580">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle12">Tel&#233;fono</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/></span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_45e8418f" >
+  <span class="textstyle11"><br/>escribe tu telefono<br/><br/><br/></span>
+  <div id="button_59fda677">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle12">Asunto</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/></span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_a218a41" >
+  <span class="textstyle11"><br/>Escribe un asunto<br/><br/></span>
+  <div id="button_741ded1e">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle10">Mensaje</span>
+        </div>
+      </div></div>
+    </div>
+  <span class="textstyle11"><br/></span>
+<input type="text" value="" title="" name="TextEdit1"  id="edit_74ba9880" >
+  <span class="textstyle11"><br/>Deja aqui tu comentario...<br/><br/><br/></span>
+  <div id="button_3b8027bb">
+    <div class="vcenterstyle1"><div class="vcenterstyle2">      <div class="textstyle1">
+        <span class="textstyle10">Enviar</span>
+        </div>
+      </div></div>
+    </div>
+<span class="textstyle11"><br/>*los campos son obligatorios.<br/><br/>B&#250;scano en facebook como: <br/>INSTITUTO T&#201;CNICO EULOGIO GALEANO TREJO<br/><br/>Ubicados en: <br/>Barrio El Farolito, Santa B&#225;rbara, Santa B&#225;rbara<br/><br/>Tel&#233;fono: 9935-3108<br/><br/>Correo electr&#243;nico:<br/>institutoeulogiog@gmail.com</span><span class="textstyle13"><br/> </span>  <span class="textstyle5"> </span>
+  <span class="textstyle14"><br/><br/></span>
+</div>
+<div style="clear:both"></div></div></div></div>
+</div><span class="textstyle9"><br/></span><span class="textstyle9"><br/></span><span class="textstyle15"><br/></span>  </div>
+</body>
+</html>
